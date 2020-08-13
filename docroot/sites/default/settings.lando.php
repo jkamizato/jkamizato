6ea1b2be37428d29e964141df0b377b3aeb0521f
Júlio <jkamizato>
2020-08-13 02:28:28 -0300

    Drupal 9 setup

---
+++ docroot/sites/default/settings.lando.php
@@ -0,0 +1,31 @@
+<?php
+
+$info = json_decode(getenv('LANDO_INFO'), TRUE);
+
+$databases = array(
+  'default' => array(
+    'default' => array(
+      'database' => $info['database']['creds']['database'],
+      'username' => $info['database']['creds']['user'],
+      'password' => $info['database']['creds']['password'],
+      'host' => $info['database']['internal_connection']['host'],
+      'port' => $info['database']['internal_connection']['port'],
+      'driver' => 'mysql',
+      'prefix' => '',
+      'collation' => 'utf8mb4_general_ci',
+    ),
+  ),
+);
+
+// This will prevent Drupal from setting read-only permissions on sites/default.
+$settings['skip_permissions_hardening'] = TRUE;
+
+// This will ensure the site can only be accessed through the intended host
+// names. Additional host patterns can be added for custom configurations.
+$settings['trusted_host_patterns'] = ['.*'];
+
+$config['environment_indicator.indicator']['name'] = 'Lando';
+$config['environment_indicator.indicator']['bg_color'] = '#ed3f7a';
+$config['environment_indicator.indicator']['fg_color'] = '#fff';
+
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
