2b29ca6ad9ada0140cf575e214e5cbd7d618dc66
Júlio <jkamizato>
2020-08-14 02:33:18 -0300

    Add drush

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -774,3 +774,6 @@ if (file_exists($app_root . '/' . $site_path . '/settings.lando.php')) {
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+
+$settings['config_sync_directory'] = '../config/sync';
